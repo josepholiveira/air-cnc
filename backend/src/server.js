a0fc4735d05f5deb3cdcb2f3b243f039f1909556
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2019-10-03 19:30:51 -0300

    Adicionando mobile

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-import path = require('path');
+const path = require("path");
 
 const routes = require("./routes");
 
@@ -17,7 +17,7 @@ mongoose.connect(
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(3333);
